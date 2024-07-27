316c5aa3a45bcc88642d2434fb79ae562ecb0fc0
mykeshale <mykeshale@gmail.com>
2024-07-27 17:25:24 +0300

    First commit

---
+++ _db.js
@@ -0,0 +1,63 @@
+let games = [
+    [
+        { id: 1, title: "Tommie", platform: ["PS5", "PS4", "XBOX"] },
+        { id: 2, title: "Glennis", platform: ["PS4"] },
+        { id: 3, title: "Darn", platform: ["PS5", "PS4"] },
+        { id: 4, title: "Dorolice", platform: ["PC", "XBOX", "PS5", "PS4"] },
+        { id: 5, title: "Englebert", platform: ["XBOX"] },
+        { id: 6, title: "Curcio", platform: ["PC"] },
+        { id: 7, title: "Baudoin", platform: ["XBOX", "PS5"] },
+        { id: 8, title: "Zora", platform: ["PC"] },
+        { id: 9, title: "Wells", platform: ["XBOX", "PS4"] },
+        { id: 10, title: "Nollie", platform: ["XBOX"] },
+        { id: 11, title: "Jo", platform: ["PS5", "PC", "PS4"] },
+        { id: 12, title: "Skip", platform: ["PS4"] },
+        { id: 13, title: "Cara", platform: ["PS5", "PS4"] },
+        { id: 14, title: "Ariel", platform: ["PS5", "XBOX", "PS4"] },
+        { id: 15, title: "Ianthe", platform: ["PS5", "PS4"] },
+        { id: 16, title: "Katha", platform: ["PS3", "XBOX"] },
+        { id: 17, title: "Derrick", platform: ["PS5"] },
+        { id: 18, title: "Joaquin", platform: ["PC", "XBOX"] },
+    ]
+];
+
+let authors = [
+
+    [
+        { id: 1, name: "Sonja", verified: true },
+        { id: 2, name: "Sonni", verified: false },
+        { id: 3, name: "Abbey", verified: false },
+        { id: 4, name: "Kelila", verified: false },
+        { id: 5, name: "Hinze", verified: true },
+        { id: 6, name: "Jakob", verified: true },
+        { id: 7, name: "Harman", verified: false },
+        { id: 8, name: "Clive", verified: false },
+        { id: 9, name: "Jeramie", verified: false },
+        { id: 10, name: "Coretta", verified: false },
+        { id: 11, name: "Gert", verified: false },
+        { id: 12, name: "Thornton", verified: false },
+        { id: 13, name: "Filmore", verified: false },
+        { id: 14, name: "Ronnica", verified: true },
+        { id: 15, name: "Porter", verified: false },
+        { id: 16, name: "Osbourne", verified: false },
+        { id: 17, name: "Ryon", verified: false },
+        { id: 18, name: "Martynne", verified: false },
+        { id: 19, name: "Harriet", verified: true },
+        { id: 20, name: "Clarey", verified: false }
+    ]
+];
+
+let reviews = [
+    [
+        { id: 1, rating: 1, content: "Monahan Group", author_id: 1, game_id: 1 },
+        { id: 2, rating: 2, content: "Vandervort-Hettinger", author_id: 2, game_id: 2 },
+        { id: 3, rating: 3, content: "Bernier-Gusikowski", author_id: 3, game_id: 3 },
+        { id: 4, rating: 4, content: "Bergnaum, Runolfsdottir and Shanahan", author_id: 4, game_id: 4 },
+        { id: 5, rating: 5, content: "Bartell, Becker and Leffler", author_id: 5, game_id: 5 },
+        { id: 6, rating: 6, content: "Sporer-Waelchi", author_id: 6, game_id: 6 },
+        { id: 7, rating: 7, content: "Kovacek Group", author_id: 7, game_id: 7 },
+        { id: 8, rating: 8, content: "Spinka, Bode and Nitzsche", author_id: 8, game_id: 8 },
+        { id: 9, rating: 9, content: "Douglas-Lockman", author_id: 9, game_id: 9 },
+        { id: 10, rating: 10, content: "Olson Group", author_id: 10, game_id: 10 }
+    ]
+]
